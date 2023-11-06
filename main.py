d4e9464df21edf92fdbe2ca79bd4cb2fea5608bd
Manue-droi <manusarf16@gmail.com>
2023-11-06 15:55:43 -0500

    upDate

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ root.geometry("300x260")
 
 # Configuración pantalla de salida 
 pantalla = Entry(root, width=40, bg="black", fg="white", borderwidth=0, font=("arial", 18, "bold"))
-pantalla.grid(row=0, column=0, columnspan=60, padx=0, pady=0)
+pantalla.grid(row=0, column=0, columnspan=60, padx=1, pady=1)
 
 # Configuración botones
 boton_1 = Button(root, text="1", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=1, column=0, padx=1, pady=1)
